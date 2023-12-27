15350bff282fd6056b385119900a1e9af1e3f641
gminiy <gmini.y@gmail.com>
2023-12-27 16:03:26 +0900

    feat: pixabay mapper 작성

---
+++ lib/mapper/pixabay_image_mapper.dart
@@ -0,0 +1,9 @@
+import 'package:image_serch_app/dto/Pixabay_image_dto.dart';
+import 'package:image_serch_app/model/pixabay_image.dart';
+
+extension ToPixabayImage on PixabayImageDto {
+  PixabayImage toPixabayImage() {
+    return PixabayImage(previewURL: previewURL ??
+        "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_150.jpg");
+  }
+}
